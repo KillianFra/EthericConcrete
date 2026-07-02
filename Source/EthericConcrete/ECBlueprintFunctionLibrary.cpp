b2912e945ea0955b434b2de5d785d0d1c826db1f
Rorsak <camvision25@gmail.com>
2026-07-02 23:01:17 +0200

    Système tir à Distance

--- Source/EthericConcrete/ECBlueprintFunctionLibrary.cpp
+++ Source/EthericConcrete/ECBlueprintFunctionLibrary.cpp
@@ -1,5 +1,10 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "ECBlueprintFunctionLibrary.h"
 
+void UECBlueprintFunctionLibrary::CancelAbilitiesByTag(UAbilitySystemComponent* ASC, FGameplayTagContainer WithTags)
+{
+	if (ASC)
+	{
+		// We pass the address of our Tag Container, leave WithoutTags null, and Ignore ability null.
+		ASC->CancelAbilities(&WithTags, nullptr, nullptr);
+	}
+}
